70da05af51415b84f00b0acfcda816496d7cf064
Hai Dang <dangcantho@gmail.com>
2025-11-23 21:21:58 +0700

    Chỉnh sửa app/Http/Controllers/Auth/LoginController.php và Thay thếtrang đăng nhập mặc định từ /login thành user/dang-nhap

--- Website thuc an nhanh/bootstrap/app.php
+++ Website thuc an nhanh/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(fn(Request $request) => route('user.dangnhap'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
