bae328526467c2efbbcdf2955ca16f123481a929
HBinh284 <hbinhagu01@gmail.com>
2025-11-23 20:06:41 +0700

    Tạo CRUD Tình trạng (CONTROLLERS & VIEWS)

--- Website thuc an nhanh/app/Http/Controllers/TinhTrangController.php
+++ Website thuc an nhanh/app/Http/Controllers/TinhTrangController.php
@@ -1,56 +1,52 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\TinhTrang;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class TinhTrangController extends Controller
 {
     public function getDanhSach()
     {
         $tinhtrang = TinhTrang::all();
-        return view('admin.tinhtrang.danhsach', compact('tinhtrang'));
+        return view('tinhtrang.danhsach', compact('tinhtrang'));
     }
 
     public function getThem()
     {
-        return view('admin.tinhtrang.them');
+        return view('tinhtrang.them');
     }
 
     public function postThem(Request $request)
     {
-        // Kiểm tra
         $request->validate([
-        'tinhtrang' => ['required', 'string', 'max:255', 'unique:tinhtrang'],
+            'tinhtrang' => ['required', 'string', 'max:255', 'unique:tinhtrang'],
         ]);
 
         $orm = new TinhTrang();
         $orm->tinhtrang = $request->tinhtrang;
         $orm->save();
 
-        // Sau khi thêm thành công thì tự động chuyển về trang danh sách
         return redirect()->route('tinhtrang');
     }
 
     public function getSua($id)
     {
         $tinhtrang = TinhTrang::find($id);
-        return view('admin.tinhtrang.sua', compact('tinhtrang'));
+        return view('tinhtrang.sua', compact('tinhtrang'));
     }
 
     public function postSua(Request $request, $id)
     {
-        // Kiểm tra
         $request->validate([
-        'tinhtrang' => ['required', 'string', 'max:255', 'unique:tinhtrang,tinhtrang,' . $id],
+            'tinhtrang' => ['required', 'string', 'max:255', 'unique:tinhtrang,tinhtrang,' . $id],
         ]);
 
         $orm = TinhTrang::find($id);
         $orm->tinhtrang = $request->tinhtrang;
         $orm->save();
 
-        // Sau khi sửa thành công thì tự động chuyển về trang danh sách
         return redirect()->route('tinhtrang');
     }
 
@@ -58,7 +54,6 @@ class TinhTrangController extends Controller
     {
         $orm = TinhTrang::find($id);
         $orm->delete();
-
-        return redirect()->route('admin.tinhtrang');
+        return redirect()->route('tinhtrang');
     }
 }
