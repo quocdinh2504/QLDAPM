067871247a036bbadb9b64298dd50db10503dca6
Hai Dang <dangcantho@gmail.com>
2025-11-24 20:44:35 +0700

    Hiển thị sản phẩm ra trang chủ và cập nhật liên kết Thêm vào giỏ hàng

--- Website thuc an nhanh/resources/views/frontend/home.blade.php
+++ Website thuc an nhanh/resources/views/frontend/home.blade.php
@@ -124,39 +124,83 @@
 		</style>
 		
 		<!-- Products (Grid) -->
-		<section class="container pt-2 mt-2 mb-3">
-			<!-- Heading -->
-			<div class="d-flex align-items-center justify-content-between border-bottom pb-3 pb-md-4">
-				<h2 class="h3 mb-0">Gà Rán</h2>
-				<div class="nav ms-3">
-					<a class="nav-link animate-underline px-0 py-2" href="#">
-						<span class="animate-target">Xem tất cả</span> <i class="ci-chevron-right fs-base ms-1"></i>
-					</a>
+		@foreach($loaisanpham as $lsp)
+			<section class="container pt-2 mt-2 mb-3">
+				<!-- Heading -->
+				<div class="d-flex align-items-center justify-content-between border-bottom pb-3 pb-md-4">
+					<h2 class="h3 mb-0">{{ $lsp->tenloai }}</h2>
+					<div class="nav ms-3">
+						<a class="nav-link animate-underline px-0 py-2" href="{{ route('frontend.sanpham.phanloai', ['tenloai_slug' => $lsp->tenloai_slug]) }}">
+							<span class="animate-target">Xem tất cả</span> <i class="ci-chevron-right fs-base ms-1"></i>
+						</a>
+					</div>
 				</div>
-			</div>
-				
-			<!-- Product grid -->
-			<!-- Phần này code quá dài nên đã cắt bớt -->
-			<!-- Giữ nguyên code phần này, chỉ cập nhật lại đường dẫn các tập tin hình ảnh -->
-			<img src="{{ asset('public/assets/img/shop/1-mieng-ga-ran.jpg') }}" />
-		</section>
 
-		<!-- Products (Grid) -->
-		<section class="container pt-2 mt-2 mb-3">
-			<!-- Heading -->
-			<div class="d-flex align-items-center justify-content-between border-bottom pb-3 pb-md-4">
-				<h2 class="h3 mb-0">Thức ăn nhẹ</h2>
-				<div class="nav ms-3">
-					<a class="nav-link animate-underline px-0 py-2" href="#">
-						<span class="animate-target">Xem tất cả</span> <i class="ci-chevron-right fs-base ms-1"></i>
-					</a>
+				<!-- Product grid -->
+				<div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-4 pt-4">
+					<!-- Item -->
+					@foreach($lsp->SanPham as $sp)
+						<div class="col">
+							<div class="product-card animate-underline hover-effect-opacity bg-body rounded">
+								<div class="position-relative">
+									<div class="position-absolute top-0 end-0 z-2 hover-effect-target opacity-0 mt-3 me-3">
+										<div class="d-flex flex-column gap-2">
+											<button type="button" class="btn btn-icon btn-secondary animate-pulse d-none d-lg-inline-flex">
+												<i class="ci-heart fs-base animate-target"></i>
+											</button>
+											<button type="button" class="btn btn-icon btn-secondary animate-rotate d-none d-lg-inline-flex">
+												<i class="ci-refresh-cw fs-base animate-target"></i>
+											</button>
+										</div>
+									</div>
+									<div class="dropdown d-lg-none position-absolute top-0 end-0 z-2 mt-2 me-2">
+										<button type="button" class="btn btn-icon btn-sm btn-secondary bg-body" data-bs-toggle="dropdown">
+											<i class="ci-more-vertical fs-lg"></i>
+										</button>
+										<ul class="dropdown-menu dropdown-menu-end fs-xs p-2" style="min-width:auto">
+											<li>
+												<a class="dropdown-item" href="#"><i class="ci-heart fs-sm ms-n1 me-2"></i> Thêm vào yêu thích</a>
+											</li>
+											<li>
+												<a class="dropdown-item" href="#"><i class="ci-refresh-cw fs-sm ms-n1 me-2"></i> So sánh</a>
+											</li>
+										</ul>
+									</div>
+									<a class="d-block rounded-top overflow-hidden p-3 p-sm-4" href="{{ route('frontend.sanpham.chitiet', ['tenloai_slug' => $lsp->tenloai_slug, 'tensanpham_slug' => $sp->tensanpham_slug]) }}">
+										<div class="ratio" style="--cz-aspect-ratio:calc(240 / 258 * 100%)">
+											<img src="{{ asset('storage/app/private/' . $sp->hinhanh) }}" />
+										</div>
+										<span class="badge bg-info position-absolute top-0 start-0 mt-2 ms-2 mt-lg-3 ms-lg-3">Mới</span>
+									</a>
+								</div>
+								<div class="w-100 min-w-0 px-1 pb-2 px-sm-3 pb-sm-3">
+									<div class="d-flex align-items-center gap-2 mb-2">
+										<div class="d-flex gap-1 fs-xs">
+											<i class="ci-star-filled text-warning"></i>
+											<i class="ci-star-filled text-warning"></i>
+											<i class="ci-star-filled text-warning"></i>
+											<i class="ci-star-filled text-warning"></i>
+											<i class="ci-star text-body-tertiary opacity-75"></i>
+										</div>
+										<span class="text-body-tertiary fs-xs">(123)</span>
+									</div>
+									<h3 class="pb-1 mb-2">
+										<a class="d-block fs-sm fw-medium text-truncate" href="{{ route('frontend.sanpham.chitiet', ['tenloai_slug' => $lsp->tenloai_slug, 'tensanpham_slug' => $sp->tensanpham_slug]) }}">
+											<span class="animate-target">{{ $sp->tensanpham }}</span>
+										</a>
+									</h3>
+									<div class="d-flex align-items-center justify-content-between">
+										<div class="h5 lh-1 mb-0">{{ number_format($sp->dongia, 0, ',', '.') }}<small>đ</small></div>
+										<a href="{{ route('frontend.giohang.them', ['tensanpham_slug' => $sp->tensanpham_slug]) }}" class="product-card-button btn btn-icon btn-secondary animate-slide-end ms-2">
+											<i class="ci-shopping-cart fs-base animate-target"></i>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					@endforeach
 				</div>
-			</div>
-
-		<!-- Product grid -->
-		<!-- Phần này code quá dài nên đã cắt bớt -->
-		<!-- Giữ nguyên code phần này, chỉ cập nhật lại đường dẫn các tập tin hình ảnh -->
-		<img src="{{ asset('public/assets/img/shop/2-mieng-ga-ran.jpg') }}" />
-		</section>
+			</section>
+		@endforeach
 	</main>
 @endsection
