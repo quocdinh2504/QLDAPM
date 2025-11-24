227a128e561f332c45ab58fe7f339b7964b452da
HuynhHuuAn <hhuuan2004@gmail.com>
2025-11-24 20:16:38 +0700

    Tạo mới, bổ sung và cập nhập code cho các trang views/user

---
+++ Website thuc an nhanh/resources/views/user/dathangthanhcong.blade.php
@@ -0,0 +1,14 @@
+	<!-- Page content -->
+	<main class="content-wrapper">
+		<section class="container mt-4 mb-4">
+			<div class="position-relative overflow-hidden rounded-5 p-4 p-sm-5" style="background-color:var(--cz-success-border-subtle)">
+				<div class="position-relative z-2 text-center py-4 py-md-5 my-md-2 my-lg-5 mx-auto" style="max-width:536px">
+					<h1 class="pt-xl-4 mb-4">Cảm ơn bạn đã đặt hàng!</h1>
+					<p class="text-dark-emphasis pb-3 pb-sm-4">Đơn hàng <span class="fw-semibold">#234000</span> đã được chấp nhận và sẽ được xử lý sớm. Dự kiến thời gian nhận hàng trước <span class="fw-semibold">Chủ Nhật, ngày 9 tháng 11 năm 2025.</span></p>
+					<a class="btn btn-lg btn-dark rounded-pill mb-xl-4" href="#">Tiếp tục mua sắm</a>
+				</div>
+				<img src="assets/img/checkout/thankyou-bg-1.png" class="animate-up-down position-absolute top-0 start-0 w-100 h-100 object-fit-cover" alt="Background image" />
+				<img src="assets/img/checkout/thankyou-bg-2.png" class="animate-down-up position-absolute top-0 start-0 w-100 h-100 object-fit-cover" alt="Background image" />
+			</div>
+		</section>
+	</main>
