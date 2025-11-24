222dff98da07d7aed5bcaecabefa4295721b6054
Hai Dang <dangcantho@gmail.com>
2025-11-24 20:52:51 +0700

    Xử lý đặt hàng

--- Website thuc an nhanh/resources/views/user/dathangthanhcong.blade.php
+++ Website thuc an nhanh/resources/views/user/dathangthanhcong.blade.php
@@ -1,3 +1,8 @@
+@extends('layouts.frontend')
+
+@section('title', 'Hoàn tất đặt hàng')
+
+@section('content')
 	<!-- Page content -->
 	<main class="content-wrapper">
 		<section class="container mt-4 mb-4">
@@ -5,10 +10,11 @@
 				<div class="position-relative z-2 text-center py-4 py-md-5 my-md-2 my-lg-5 mx-auto" style="max-width:536px">
 					<h1 class="pt-xl-4 mb-4">Cảm ơn bạn đã đặt hàng!</h1>
 					<p class="text-dark-emphasis pb-3 pb-sm-4">Đơn hàng <span class="fw-semibold">#234000</span> đã được chấp nhận và sẽ được xử lý sớm. Dự kiến thời gian nhận hàng trước <span class="fw-semibold">Chủ Nhật, ngày 9 tháng 11 năm 2025.</span></p>
-					<a class="btn btn-lg btn-dark rounded-pill mb-xl-4" href="#">Tiếp tục mua sắm</a>
+					<a class="btn btn-lg btn-dark rounded-pill mb-xl-4" href="{{ route('frontend.home') }}">Tiếp tục mua sắm</a>
 				</div>
-				<img src="assets/img/checkout/thankyou-bg-1.png" class="animate-up-down position-absolute top-0 start-0 w-100 h-100 object-fit-cover" alt="Background image" />
-				<img src="assets/img/checkout/thankyou-bg-2.png" class="animate-down-up position-absolute top-0 start-0 w-100 h-100 object-fit-cover" alt="Background image" />
+				<img src="{{ asset('public/assets/img/checkout/thankyou-bg-1.png') }}" class="animate-up-down position-absolute top-0 start-0 w-100 h-100 object-fit-cover" alt="Background image" />
+				<img src="{{ asset('public/assets/img/checkout/thankyou-bg-2.png') }}" class="animate-down-up position-absolute top-0 start-0 w-100 h-100 object-fit-cover" alt="Background image" />
 			</div>
 		</section>
 	</main>
+@endsection
